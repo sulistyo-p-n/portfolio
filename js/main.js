c6646dcb7cac395f1b09876a52c309d9e97a9d8c
UIOUIOUIO <sulistyo.p.n@gmail.com>
2025-02-26 14:28:25 +0700

    fix smooth scrolling

--- js/main.js
+++ js/main.js
@@ -339,8 +339,21 @@
             container: window
         }, easeFunctions);
 
-        triggers.forEach(function(trigger) {
-            moveTo.registerTrigger(trigger);
+        // triggers.forEach(function(trigger) {
+        //     moveTo.registerTrigger(trigger);
+        // });
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    window.scrollTo({
+                        top: target.offsetTop,
+                        behavior: 'smooth'
+                    });
+                }
+            });
         });
 
     }; // end ssMoveTo
